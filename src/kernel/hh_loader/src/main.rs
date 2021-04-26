4c23e923d7e703751691c40c148251c5553f9be0
Marco Cicognani <marco.cicognani@meetixos.org>
2021-04-26 17:25:52 +0200

    HH_Loader: Added version

--- src/kernel/hh_loader/src/main.rs
+++ src/kernel/hh_loader/src/main.rs
@@ -6,11 +6,12 @@
 use hal::{boot::infos::BootInfos, paging::PageDir};
 use logger::info;
 
-use crate::log::init_logger;
+use crate::{log::init_logger, version::HHL_VERSION};
 
 mod arch;
 mod log;
 mod panic;
+mod version;
 
 /* includes the module which links the kernel core binary */
 include!(env!("KERNEL_BIN"));
@@ -23,13 +24,13 @@ pub unsafe extern "C" fn hhl_rust_entry(raw_info_ptr: *const u8) -> ! {
     /* initialize the logger */
     init_logger();
 
+    info!("MeetiX Kernel Loader v{}", HHL_VERSION);
+    info!("Kernel size: {}", KERNEL_SIZE);
+    info!("Kernel bytes[0]: {}", KERNEL_BYTES[0]);
+
     info!("Raw info ptr: {:#x}", raw_info_ptr as usize);
     boot_info.cmdline_args().iter().for_each(|arg| info!("Arg: {}", arg.as_str()));
     boot_info.mem_areas().iter().for_each(|mem_area| info!("{:?}", mem_area));
-
-    info!("MeetiX Kernel Loader v0.1.0");
-    info!("Kernel size: {}", KERNEL_SIZE);
-    info!("Kernel bytes[0]: {}", KERNEL_BYTES[0]);
 
     let page_dir = PageDir::active_page_dir(0);
     info!("\n{:?}", page_dir);
---
+++ src/kernel/hh_loader/src/version.rs
@@ -0,0 +1,6 @@
+/*! # Kernel Version
+ *
+ * Lists the kernel version
+ */
+
+pub const HHL_VERSION: &'static str = env!("CARGO_PKG_VERSION");
