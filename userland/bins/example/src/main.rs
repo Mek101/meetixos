603de954966f75fe011f90c6e825e200153ab4bf
Marco Cicognani <marco.cicognani@meetixos.org>
2021-06-02 23:59:18 -0700

    Bins/Example: Updated example

--- userland/bins/example/src/main.rs
+++ userland/bins/example/src/main.rs
@@ -3,23 +3,44 @@
 
 use core::panic::PanicInfo;
 
-use api::objs::{
-    impls::file::File,
-    object::{
-        Object,
-        UserCreatable
+use api::{
+    bits::path::PathExistsState,
+    objs::{
+        impls::file::File,
+        object::{
+            Object,
+            UserCreatable
+        }
+    },
+    path::Path,
+    tasks::{
+        impls::thread::Thread,
+        task::Task
     }
 };
 
 #[no_mangle]
 pub unsafe extern "C" fn _start() {
-    let f = File::creat().for_read()
-                         .for_write()
-                         .apply_for("/Users/Marco/Docs/example.txt")
-                         .unwrap();
+    let file_path = Path::from("/Users/Marco/Docs/example.txt");
+    let file = match file_path.exists() {
+        PathExistsState::Exists(_) => File::open().for_read()
+                                                  .for_write()
+                                                  .apply_for(file_path)
+                                                  .expect("Failed to open"),
+        PathExistsState::NotExists => File::creat().for_read()
+                                                   .for_write()
+                                                   .apply_for(file_path)
+                                                   .expect("Failed to create"),
+        PathExistsState::ExistsUntil(_) | PathExistsState::EmptyPath => {
+            panic!("Cannot create {}", file_path)
+        }
+    };
 
-    let mmap = f.map_to_memory(None, 0, f.size() as u64, true)
-                .expect("Failed to map file to memory");
+    let file_size =
+        file.info().expect("Failed to retrieve file size").mem_info().used_size();
+
+    let mmap = file.map_to_memory(None, 0, file_size as u64, true)
+                   .expect("Failed to map file to memory");
 
     let mut ptr_box = mmap.get_ptr_mut::<u8>().expect("Failed to obtain MMap pointer");
     for byte in ptr_box.iter_mut() {
@@ -27,6 +48,8 @@ pub unsafe extern "C" fn _start() {
     }
 
     /* cannot do anything for now :-( */
+    Thread::this().terminate(true).expect("Failed to exit");
+    unreachable!();
 }
 
 #[panic_handler]
