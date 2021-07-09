34af01dde98bf9fc43b5f922a5e7e7077a206338
Marco Cicognani <marco.cicognani@meetixos.org>
2021-07-09 12:01:17 +0200

    Kernel: Fixed compilation warnings about un-aligned access to the TSS structure

--- Kernel/src/arch/x86_64/descs/tss.rs
+++ Kernel/src/arch/x86_64/descs/tss.rs
@@ -12,13 +12,13 @@ use crate::addr::{
 #[derive(Copy, Clone)]
 pub struct TaskStateSegment {
     _reserved_field_1: u32,
-    m_stacks_per_privilege: [VirtAddr; 3],
+    pub m_stacks_per_privilege: [VirtAddr; 3],
     _reserved_field_2: u32,
-    m_full_intr_stack_table: [VirtAddr; 7],
+    pub m_full_intr_stack_table: [VirtAddr; 7],
     _reserved_field_3: u32,
     _reserved_field_4: u32,
     _reserved_field_5: u16,
-    m_io_map_base: u16
+    pub m_io_map_base: u16
 }
 
 impl TaskStateSegment {
@@ -34,42 +34,5 @@ impl TaskStateSegment {
                _reserved_field_4: 0,
                _reserved_field_5: 0,
                m_io_map_base: 0 }
-    }
-
-    /**
-     * Returns the slice of 64bit canonical stack pointers for privilege
-     * levels from 0 to 2
-     */
-    pub fn stacks_per_privilege(&self) -> &[VirtAddr] {
-        self.m_stacks_per_privilege.as_slice()
-    }
-
-    /**
-     * Returns the mutable slice of 64bit canonical stack pointers for
-     * privilege levels from 0 to 2
-     */
-    pub fn stack_per_privilege_mut(&mut self) -> &mut [VirtAddr] {
-        self.m_stacks_per_privilege.as_mut_slice()
-    }
-
-    /**
-     * Returns the slice of 64bit canonical interrupt stack pointers
-     */
-    pub fn full_intr_stack_table(&self) -> &[VirtAddr] {
-        self.m_full_intr_stack_table.as_slice()
-    }
-
-    /**
-     * Returns the mutable slice of 64bit canonical interrupt stack pointers
-     */
-    pub fn full_intr_stack_table_mut(&mut self) -> &mut [VirtAddr] {
-        self.m_full_intr_stack_table.as_mut_slice()
-    }
-
-    /**
-     * Returns the base for the x86 I/O map
-     */
-    pub fn io_map_base(&self) -> u16 {
-        self.m_io_map_base
     }
 }
